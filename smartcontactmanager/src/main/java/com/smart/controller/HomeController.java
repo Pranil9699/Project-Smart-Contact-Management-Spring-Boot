95eaf6b3c87ff302fcf2db44f3742dc302a01e57
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2023-09-13 22:18:04 +0530

    Add files via upload

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import com.smart.dao.ForgotPasswordRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.ForgotPassword;
+import com.smart.entities.Location;
 import com.smart.entities.User;
 import com.smart.helper.Message;
 import com.smart.services.EmailService;
@@ -50,6 +54,76 @@ public class HomeController {
 		m.addAttribute("title", "About - Smart Contact Manager");
 		return "about";
 	}
+	
+	@PostMapping("/getPlace")
+	 public String locationSubmit(@ModelAttribute Location location, BindingResult bindingResult, @RequestParam("name") String name) {
+		 
+		 
+//			RestTemplate restTemplate = new RestTemplate();
+            RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> topic_body = restTemplate.exchange("https://nominatim.openstreetmap.org/?addressdetails=1&q="+location.getName()+"&format=json&limit=1", 
+					HttpMethod.GET, null, String.class );//comsuming openstreetmap api
+					 
+			String  topics = topic_body.getBody(); 
+			System.out.println(topics);
+			topics = topics.replace("\"address\":{", "");
+			topics = topics.replace("[","");
+			topics = topics.replace("]","");
+			topics = topics.replace("}}","");
+			topics = topics.replace("{","");
+			
+			List<String> test = new ArrayList<String>();
+			
+			String[] list = topics.split(",\"");
+			Location l = new Location();
+			
+			for (int i = 0; i < list.length; i++) {
+				
+				String j =list[i].replace("\"", "");
+				list[i] = j;
+
+				 String[] list1 = list[i].split(":");
+				 
+				
+				if (list1[0].equals("lat")  ) {
+					test.add(list1[1]);
+				}
+				if ( list1[0].equals("lon") ) {
+								
+					test.add(list1[1]);
+							}
+				if ( list1[0].equals("place_id") ) {
+					
+					test.add(list1[1]);
+				}
+				if ( list1[0].equals("country") ) {
+					
+					test.add(list1[1]);
+				}
+			}
+			System.out.println(test);
+			location.setCountry(test.get(3));
+			location.setPlace_id(test.get(0));
+			location.setLatitude(test.get(1));
+			location.setLongitud(test.get(2));
+			
+			System.out.println(location);
+			return "details";
+	  }
+	 
+	 @GetMapping("/map")
+	 public String locationSubmit(Model model) {
+		 
+			model.addAttribute("Location", new Location());
+		
+			System.out.println("Done");
+			return "locationInterface";
+	  }
+	 
+	 
+	 
+	 
+	 
 
 	@GetMapping("/signup")
 	public String SignUp(Model m) {
